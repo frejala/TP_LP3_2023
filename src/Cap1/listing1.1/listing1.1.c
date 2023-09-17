b2e4de9d61e4c5fac2b6cbb42b33fdb6666bd955
frejala <fa.rejala03@gmail.com>
2023-09-17 18:45:51 -0400

    First listings

---
+++ src/Cap1/listing1.1/listing1.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../listing1.3/listing1.3.hpp"
+
+int main(int argc, char **argv)
+{
+    int i;
+    i = atoi(argv[1]);
+    printf("The reciprocal of %d is %g\n", i, reciprocal(i));
+    return 0;
+}
